af7cd21501a1049f9ab354209e7febc0690ec4a0
S3takuto <lingmutaren600@gmail.com>
2024-05-10 20:54:54 +0900

    make function

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
 from flask import Flask
+import defs
 
 app = Flask(__name__)
+
 @app.route("/")
 def index1():
     return "index1"
 
 @app.route("/2")
 def index2():
-    return "index2"
+    return defs.func()
 
 if __name__ == "__main__":
     app.run(debug=False)
