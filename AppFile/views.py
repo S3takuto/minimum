4c1a46f26ed5056aafe3a72561b9510a92d58cf7
S3takuto <lingmutaren600@gmail.com>
2024-05-12 10:15:23 +0900

    add html files

--- AppFile/views.py
+++ AppFile/views.py
@@ -1,9 +1,14 @@
+from flask import render_template
 from AppFile import app, defs
 
 @app.route("/")
 def index1():
-    return "index1"
+    return render_template("./HTMLFile/home.html")
 
 @app.route("/2")
 def index2():
-    return defs.func()
+    return render_template("./HTMLFile/upload.html", STR=defs.func())
+
+@app.route("/3")
+def setting(method=['POST', 'GET']):
+    return "setting"
--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from AppFile import app
 
 if __name__ == "__main__":
